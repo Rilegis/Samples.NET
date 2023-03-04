2971122f9e85985e86c6e89d3915d0272c7674bb
Rilegis <13032631+Rilegis@users.noreply.github.com>
2023-03-04 21:36:14 +0100

    Added main program loop to execute a given addon.

--- Reflection/AppWithAddons/AppWithAddons.App/Program.cs
+++ Reflection/AppWithAddons/AppWithAddons.App/Program.cs
@@ -10,7 +10,11 @@
     Date        Author      Ref     Revision 
     04/03/2023  Rilegis     1       First code commit.
     04/03/2023  Rilegis     2       Added "AddonsDirectory" existence check.
+    04/03/2023  Rilegis     3       Added main program loop to execute a given addon.
 **********************************************************************/
+
+using AppWithAddons.App.Handlers;
+using AppWithAddons.SDK;
 
 namespace AppWithAddons.App
 {
@@ -29,6 +33,37 @@ namespace AppWithAddons.App
                     Console.WriteLine($"No addons directory found...creating '{Constants.AddonsDirectory}' directory.");
                     Directory.CreateDirectory(Constants.AddonsDirectory);
                 }
+
+                // Load addons
+                AddonsHandler.Addons = AddonsHandler.LoadAddons(); // Might be changed to AddonsHandlers ah = new(); and do addon loading/initialization inside the constructor
+                AddonsHandler.InitializeAddons();
+
+                while (true)
+                {
+                    Console.WriteLine($"Currently loaded addons:");
+                    foreach (KeyValuePair<int, IAddon> addon in AddonsHandler.Addons)
+                    {
+                        Console.WriteLine($"{addon.Key}\t{addon.Value.Name}");
+                    }
+                    Console.Write("Select an addon to run, or type 'q' to quit: ");
+                    string selection = Console.ReadLine();
+                    if (selection.ToLower().Equals("q"))
+                        break;
+                    else
+                    {
+                        if (int.TryParse(selection, out int id))
+                        {
+                            if (AddonsHandler.Addons.TryGetValue(id, out IAddon addon))
+                                addon.Run();
+                            else
+                                Console.WriteLine("Invalid selection.");
+                        }
+                        else
+                            Console.WriteLine("Invalid input.");
+                    }
+                }
+
+                AddonsHandler.TerminateAddons(); // Might be changed to ah.Dispose(); and do addon termination inside the deconstructor.
             }
             catch (Exception ex)
             {
