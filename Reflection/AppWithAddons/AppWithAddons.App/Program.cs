0f2ab2b48aeacdc332ef4ca3d7b98cd9a9a60923
Rilegis <13032631+Rilegis@users.noreply.github.com>
2023-03-04 17:47:43 +0100

    Added "AddonsDirectory" existence check.

--- Reflection/AppWithAddons/AppWithAddons.App/Program.cs
+++ Reflection/AppWithAddons/AppWithAddons.App/Program.cs
@@ -9,6 +9,7 @@
     Revision History  :
     Date        Author      Ref     Revision 
     04/03/2023  Rilegis     1       First code commit.
+    04/03/2023  Rilegis     2       Added "AddonsDirectory" handling.
 **********************************************************************/
 
 namespace AppWithAddons.App
@@ -17,7 +18,22 @@ namespace AppWithAddons.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine($"Starting execution of application '{typeof(Program).Namespace}'");
+
+                // Check if 'addons' directory exists;
+                // If it does not exists then create it, and proceed to load the plugins
+                if (!Directory.Exists(Constants.AddonsDirectory))
+                {
+                    Console.WriteLine($"No addons directory found...creating '{Constants.AddonsDirectory}' directory.");
+                    Directory.CreateDirectory(Constants.AddonsDirectory);
+                }                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[RUNTIME EXCEPTION] {ex.Message}");
+            }
         }
     }
 }
