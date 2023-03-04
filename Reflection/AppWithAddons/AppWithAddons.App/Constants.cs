644a8e3f59022798c5ca27c8c38faba3f8e7f327
Rilegis <13032631+Rilegis@users.noreply.github.com>
2023-03-04 17:33:27 +0100

    Class definition and "AddonsDirectory" constant definition.

---
+++ Reflection/AppWithAddons/AppWithAddons.App/Constants.cs
@@ -0,0 +1,21 @@
+﻿/**********************************************************************
+    Author            : Rilegis
+    Repository        : Samples.NET
+    Project           : AppWithAddons.App
+    File name         : Constants.cs
+    Date created      : 04/03/2023
+    Purpose           : This static class contains basic constant properties needed for the app's execution.
+
+    Revision History  :
+    Date        Author      Ref     Revision 
+    04/03/2023  Rilegis     1       Class definition and 'AddonsDirectory' constant definition.
+**********************************************************************/
+
+namespace AppWithAddons.App
+{
+    internal static class Constants
+    {
+        // Directory that will contain all addons
+        internal const string AddonsDirectory = @".\addons";
+    }
+}
