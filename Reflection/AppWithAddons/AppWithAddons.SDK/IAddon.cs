9e60a882f4079d8f35232d71875804703677f9dd
Rilegis <13032631+Rilegis@users.noreply.github.com>
2023-03-04 20:02:23 +0100

    Addes some basic summaries.

--- Reflection/AppWithAddons/AppWithAddons.SDK/IAddon.cs
+++ Reflection/AppWithAddons/AppWithAddons.SDK/IAddon.cs
@@ -12,20 +12,57 @@
     Date        Author      Ref     Revision 
     04/03/2023  Rilegis     1       First code commit.
     04/03/2023  Rilegis     2       Interface definition.
+    04/03/2023  Rilegis     3       Added some basic summaries.
 **********************************************************************/
 
 namespace AppWithAddons.SDK
 {
+    /// <summary>
+    /// Addons interface implementation.
+    /// Every addon needs to implement this interface.
+    /// </summary>
     public interface IAddon
     {
+        /// <summary>
+        /// Addon's fancy name.
+        /// </summary>
         public string Name { get; }
+
+        /// <summary>
+        /// Addon's description.
+        /// </summary>
         public string Description { get; }
+
+        /// <summary>
+        /// Addon's author.
+        /// </summary>
         public string Author { get; }
+
+        /// <summary>
+        /// Addon's author URL.
+        /// </summary>
         public string AuthorUrl { get; }
+
+        /// <summary>
+        /// Addons version string.
+        /// </summary>
         public string Version { get; }
 
+        /// <summary>
+        /// Addon's initialization method.
+        /// This method will be called only once, right after the loading sequence.
+        /// </summary>
         public void Initialize();
+
+        /// <summary>
+        /// This method will be called every time the addons receives an execution request.
+        /// </summary>
         public void Run();
+
+        /// <summary>
+        /// Addon's termination method.
+        /// This method will be called only once, right before the app's shutdown.
+        /// </summary>
         public void Terminate();
     }
 }
